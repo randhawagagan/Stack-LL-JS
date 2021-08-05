92c4c9685ea29c107ce109a52d7ca0d4183131f6
Gagan Singh <randhawa_gagan@live.com>
2021-08-05 06:39:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack{
+  constructor(){
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  peek(){
+      return this.top;
+  }
+
+  push(value){
+    const newNode = new Node(value);
+    if(this.length === 0 ){
+    this.top = newNode;
+    this.bottom = newNode;
+    }else{
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop(){
+    if(!this.top){
+    return null;
+    }
+    if(this.top === this.bottom){
+      this.bottom = null;
+    }
+      // const unWantedNode = this.top; if needed to return then take reference otherwise just pop
+      this.top = unWantedNode.next;
+    this.length--;
+  return this;
+  }
+
+
+}
+
+const myStack = new Stack();
+
+myStack.push(1);
+myStack.push(5);
+myStack.push(10);
+myStack.push(17);
+myStack.peek();
+myStack.pop();
+myStack.pop();
+// myStack.peek();
